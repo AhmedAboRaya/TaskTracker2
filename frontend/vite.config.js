143bf8c70a4d099b2a7353ce81d30cfc525c4ec1
Ahmed Abo-Raya <88713925+AhmedAboRaya@users.noreply.github.com>
2024-10-19 10:59:51 +0300

    try

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
+// Load environment variables from .env file
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  server: {
+    port: process.env.PORT || 3000,
+    open: true,
+  },
 });
